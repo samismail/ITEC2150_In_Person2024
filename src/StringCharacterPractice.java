8dd7648d7df85fe72bfd843659a4330504c04edf
samismail <sismail1@ggc.edu>
2024-01-12 11:39:38 -0500

    update

---
+++ src/StringCharacterPractice.java
@@ -0,0 +1,9 @@
+public class StringCharacterPractice {
+    public static void main(String[] args) {
+        String message = "Hello World";
+        System.out.println(message.length());
+
+        String message2 = "Trying to find right in my wrongs hoping my legacy lives on";
+        System.out.println(message2.charAt(10));
+    }
+}
---
+++ src/VariablePractice.java
@@ -0,0 +1,11 @@
+public class VariablePractice {
+    public static void main(String[] args) {
+        int temperature = 25;
+        System.out.println(temperature);
+
+        String firstName = "Sam";
+        String lastName = "Ismail";
+
+        System.out.println(firstName + " " + lastName);
+    }
+}
