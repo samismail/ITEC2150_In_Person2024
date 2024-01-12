c2217f96a14d447d74268294c70b770611e74b3e
Sam <sismail1@ggc.edu>
2024-01-12 17:19:52 -0500

    Loop1 update

--- src/LoopPractice1.java
+++ src/LoopPractice1.java
@@ -1,15 +1,10 @@
 public class LoopPractice1 {
     public static void main(String[] args) {
-        int i = 0;
 
-        public static int loop(int i) {
             for (int i = 1; i <= 10; i++) {
                 if (i % 2 == 0) {
-                    return i;
-                } else {
-                    return;
+                    System.out.println(i);
                 }
             }
         }
     }
-}
