7317d3b790ff225adbf7593a33d03167aa57259e
samismail <sismail1@ggc.edu>
2024-02-07 12:21:04 -0500

    In class 2/7

---
+++ src/InClass.java
@@ -0,0 +1,9 @@
+public class InClass {
+        public static void main(String[] args) {
+
+            String str1 = "Hello";
+            String str2 = str1;
+            String  str3 = new String("Hello");
+            System.out.println(str1 == str3);
+        }
+}
