3d517f3003115b8a3b9a128b59c40747e5bdbb19
samismail <79952602+samismail@users.noreply.github.com>
2024-02-28 20:55:12 -0500

    Update ProductSales.java

--- src/Sales/ProductSales.java
+++ src/Sales/ProductSales.java
@@ -5,13 +5,13 @@ public class ProductSales {
 
         Sale s1 = new Sale("Power Generator", 10.00);
         DiscountSale ds1 = new DiscountSale("Power Generator", 10.0,5);
-        double newPrice = s1.totalPrice() - ds1.
-
-        System.out.println("This is a good deal you are saving $" + );
+        double newPrice = s1.totalPrice() - ds1.totalPrice();
 
         System.out.println(s1);
         System.out.println(s1.totalPrice());
         System.out.println(ds1);
+        System.out.println("This is a good deal you are saving $" + newPrice);
+
         System.out.println(s1.greaterThan(ds1));
         System.out.println(s1.lessThan(ds1));
         System.out.println(s1.equalDeals(ds1));
