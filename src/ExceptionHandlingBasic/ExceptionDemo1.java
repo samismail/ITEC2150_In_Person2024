00f7b52e584a3ff59edb26aa385812a58493d2e3
samismail <sismail1@ggc.edu>
2024-03-25 12:18:56 -0400

    In class 3/25

--- src/ExceptionHandlingBasic/ExceptionDemo1.java
+++ src/ExceptionHandlingBasic/ExceptionDemo1.java
@@ -4,15 +4,17 @@ public class ExceptionDemo1 {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        try{
-            System.out.println("Enter an integer");
-            int number = input.nextInt();
-            System.out.println("You entered " + number);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        finally {
-            System.out.println("////END OF PROGRAM////");
-        }
+        boolean flag = true;
+        do {
+            try {
+                System.out.println("Enter an integer");
+                int number = input.nextInt();
+                flag = false;
+                System.out.println("You entered " + number);
+            } catch (Exception e) {
+                System.out.println("You entered invalid type. Please enter an integer");
+                input.nextInt();
+            }
+        } while (flag);
     }
 }
